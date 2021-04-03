4f080519fa704f624ade79d1fade5702558a3619
Rostislav <suslovros@gmail.com>
2021-04-04 00:47:52 +0300

    git add .

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+ import React from 'react';
+
+ const Navbar =()=> {
+   return (
+    <nav className="nav">
+            <a href="#" className="nav-link">Profile</a>
+            <a href="#" className="nav-link">Massages</a>
+            <a href="#" className="nav-link">News</a>
+            <a href="#" className="nav-link">Music</a>
+            <a href="#" className="nav-link">Settings</a>
+        </nav>
+   )
+ }
+
+ export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+ import React from 'react';
+
+ const Profile =()=> {
+   return (
+    <div className="content">
+          <div className="background-line">
+            <img src="https://www.nationsonline.org/gallery/UAE/Desert-road-UAE.jpg"  className="background-line__img" />
+          </div>
+          <div>
+            <div className='ava-box'>
+                <img src="https://lh3.googleusercontent.com/proxy/6nv6qkeJrOcWbx16kRWruyGUdyG0pDHUVzyw-iv6L35LVcHECntqjF2Iv1Y6WEvrn-1XsdsQFjoE"  className="ava-box__img" />
+            </div>
+            <div className="description">
+            description lorem10 description lorem10 description lorem10 description lorem10 description lorem10 description lorem10 
+            </div>
+          </div>
+          <div> 
+            new post
+          </div>
+        </div>
+   )
+ }
+
+ export default Profile;
